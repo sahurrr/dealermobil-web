c0c49c92aa48186236a70ca00bb36ef844cf263e
sahurrr <gfadhil103@gmail.com>
2025-07-12 13:09:53 +0700

    Fix app run host and port for Railway

--- app.py
+++ app.py
@@ -115,5 +115,5 @@ def checkout():
 # ------------------------ JALANKAN APP ------------------------
 if __name__ == '__main__':
     import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get('PORT', 5000))  # Gunakan PORT dari Railway
+    app.run(host='0.0.0.0', port=port)        # Penting: host 0.0.0.0
